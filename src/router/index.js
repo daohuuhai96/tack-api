f91cdeba2b57efdbb2339f1833f7d6330ce5a72b
daohuuhai96@gmail.com <daohuuhai96@gmail.com>
2021-04-03 22:52:33 +0700

    upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import Home from '../components/HelloWorld'
 import user from './modules/user'
 
-export default [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    beforeEnter: requireAuthenticated
-  },
-  ...user
+export default [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+        beforeEnter: requireAuthenticated
+    },
+    ...user
 ]
 
 function requireAuthenticated(to, from, next) {
-  const loggedIn = localStorage.getItem('user');
+    const loggedIn = localStorage.getItem('user');
 
-  if (!loggedIn) {
-    return next('/login');
-  }
+    if (!loggedIn) {
+        return next('/login');
+    }
 
-  next();
+    next();
 }
